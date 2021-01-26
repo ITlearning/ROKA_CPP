a68497bcf42c952c0f20c9c87e0296a74d27ade0
ITlearning <yo7504@naver.com>
2021-01-26 14:23:25 +0000

    01월 26일 중간 커밋

---
+++ 2021_01/2021_01_26/Ex06.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+char& find(char a[], char c, bool& success) {
+	int num = 0;
+	for(int i = 0; i < strlen(a); i++) {
+		if(a[i] == c) {
+			cout << a[i] << ' ' << c << endl;
+			num = i;
+			success = true;
+		}
+	}
+	
+	return a[num];
+}
+
+int main() {
+	char s[] = "Mike";
+	bool b = false;
+	char& loc = find(s, 'M', b);
+	if(b == false) {
+		cout << "M을 발견할 수 없다." << endl;
+		return 0;
+	}
+	loc = 'm';
+	cout << s << endl;
+	
+}
