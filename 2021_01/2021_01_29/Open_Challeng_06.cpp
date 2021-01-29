9678c887807d053045cffaa160e27d81204b2b75
ITlearning <yo7504@naver.com>
2021-01-29 14:49:06 +0000

    01월 29일 최종 커밋

---
+++ 2021_01/2021_01_29/Open_Challeng_06.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+class UpDownGame {
+	// static 공유 랜덤 생성 번호
+	// 스타트
+	
+	static int RandomNum;
+}
+
+int UpDownGame::RandomNum = 
+
+class Person {
+	// 참여자 이름
+	// 참여자 명수
+	// 참여자가 외친 숫자
+	string Name;
+	string *p;
+	int num;
+	public:
+	Person(int a) { p = new Person[a]; }
+}
+
+int main() {
+	int a;
+	cout << "참여자수를 입력하세요 : ";
+	cin >> a;
+	
+}
