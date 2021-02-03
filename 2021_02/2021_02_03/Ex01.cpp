5295347bf112b28635bd3bd3d29a39f005a2e664
ITlearning <yo7504@naver.com>
2021-02-03 14:35:41 +0000

    02월 03일 최종 커밋

---
+++ 2021_02/2021_02_03/Ex01.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Book {
+	string title;
+	int price, pages;
+public:
+	Book(string title ="", int price = 0, int pages = 0) {
+		this->title = title;
+		this->price = price;
+		this->pages = pages;
+	}
+	void show() {
+		cout << title << ' ' << price << "원 " << pages << " 페이지" << endl;
+	}
+	string getTitle() { return title; }
+	friend Book operator+=(Book& op,int a);
+	friend Book operator-=(Book& op,int a);
+};
+
+
+Book operator+=(Book& op,int a) {
+		op.price += a;
+		return op;
+}
+
+Book operator-=(Book& op,int a) {
+		op.price -= a;
+		return op;
+}
+
+int main() {
+	Book a("청춘", 20000, 300), b("미래", 30000, 500);
+	a += 500;
+	b -= 500;
+	a.show();
+	b.show();
+}
