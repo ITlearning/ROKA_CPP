15104b1a2e15cb09d3d7d2a25ac244f2f4f6aab8
ITlearning <yo7504@naver.com>
2021-02-12 14:57:24 +0000

    OP09 중간 커밋

--- 2021_02/2021_02_12/Open_Challenge09.cpp
+++ 2021_02/2021_02_12/Open_Challenge09.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
 class GameObject {
@@ -28,31 +29,165 @@ public:
 
 class Human : public GameObject {
 	string m;
+	bool t,f;
 public:
+	Human(int startX, int startY, int distance) : GameObject(startX, startY, distance) { t = false; f= false; }
 	void move() {
+		
 		cout << "왼쪽(a), 아래(s), 위(d), 오른쪽(f) >> ";
 		cin >> m;
 		if(m == "a") {
-			x -= distance;
+			if(x <= 0) {
+				x = 0;
+			} else if (x >= 19) {
+				x = 19;
+			} else {
+				x -= distance;
+			}
 		} else if( m == "s"){
-			y -= distance;
+			if(y <= 0) {
+				y = 0;
+			} else if (y >= 21) {
+				y = 20;
+			} else {
+				y += distance;
+			}
 		} else if(m == "d") {
-			x += distance;
+			if(x <= 0) {
+				if(t == false) {
+					x += distance;
+					t = true;
+				}else {
+					x = 0;
+				}
+			} else if (x >= 19) {
+				x = 19;
+			} else {
+				
+			}
+			
 		} else if(m == "f") {
-			y += distance;
+			if(y <= 0) {
+				if(t == false) {
+					y += distance;
+					t = true;
+				}else {
+					y = 0;
+				}
+			} else if (y >= 19) {
+				y = 19;
+			} else {
+				y -= distance;
+			}
+			
 		}
+	}
+	
+	char getShape() {
+		return 'H';
 	}
 	
 };
 
 class Monster : public GameObject {
-	
+public:
+	Monster(int startX, int startY, int distance) : GameObject(startX, startY, distance) {}
+	void move() {
+		srand((unsigned int)time(0));
+		
+		int num = rand() % 4;
+		if(num == 0) {
+			x -= distance;
+		} else if(num == 1){
+			y -= distance;
+		} else if(num == 2) {
+			x += distance;
+		} else if(num == 3) {
+			y += distance;
+		}
+	}
+
+	char getShape() {
+		return 'M';
+	}
 };
 
 class Food : public GameObject {
+	int cnt;
+public:
+	Food(int startX, int startY, int distance) : GameObject(startX, startY, distance) { cnt = 0; }
+	void move() {
+		
+		if(cnt > 3) {
+			if(cnt == 5) {
+				cnt = 0;
+			}else {
+				int num = rand() % 4;
+				if(num == 0) {
+					x -= distance;
+				} else if(num == 1){
+					y -= distance;
+				} else if(num == 2) {
+					x += distance;
+				} else if(num == 3) {
+					y += distance;
+				}
+			}
+				cnt++;
+		}else {
+			cnt++;
+		}
+	}
 	
+	char getShape() {
+		return '@';
+	}
 };
 
 int main() {
+	int cnt = 0;
+	Human H(0,0,1);
+	Monster M(5,5,2);
+	Food F(9,9,1);
+	cout << "** Human의 Food 먹기 게임을 시작합니다. **" << '\n';
+	int j = 0;
+	string Game[10][20];
+	for(int i = 0; i < 10; i++) {
+		j = 0;
+		for(; j < 20; j++){
+			Game[i][j] = "-";
+		}
+	}
+	Game[H.getX()][H.getY()] = H.getShape();
+	Game[M.getX()][M.getY()] = M.getShape();
+	Game[F.getX()][F.getY()] = F.getShape();
+	while(true) {
+		
+		Game[H.getX()][H.getY()] = H.getShape();
+		Game[M.getX()][M.getY()] = M.getShape();
+		Game[F.getX()][F.getY()] = F.getShape();
+		for(int i = 0; i < 10; i++) {
+		j = 0;
+		for(; j < 20; j++){
+			cout << Game[i][j];
+		}
+		cout << '\n';
+		}
+		cout << '\n';
+		int px = H.getX();
+		int py = H.getY();
+		int mx = M.getX();
+		int my = M.getY();
+		int fx = F.getX();
+		int fy = F.getY();
+		Game[px][py] = "-";
+		Game[mx][my] = "-";
+		Game[fx][fy] = "-";
+		H.move();
+		M.move();
+		F.move();
+		
+	}
 	
+
 }
