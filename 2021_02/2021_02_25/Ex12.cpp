8aaddc80c31584c6f251a26daed2ffbd967acf95
ITlearning <yo7504@naver.com>
2021-02-26 10:10:03 +0000

    02월 26일 중간 커밋

--- 2021_02/2021_02_25/Ex12.cpp
+++ 2021_02/2021_02_25/Ex12.cpp
@@ -1,162 +1,164 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
-class Coffee {
-	string CoffeeName;
-	int size;
-	int cup;
-	int water;
+class Product {
+protected:
+	string name;
+	int count;
 public:
-	Coffee(string CoffeeName = "", int size = 3) {
-		this->CoffeeName = CoffeeName;
-		this->size = size;
-		cup = 3;
-		water = 3;
-	}
-	
-	void printNo() {
-		cout << "Water" << '\t';
-		for(int i = 0; i < water; i++) {
-			cout << "*";
-		}
-		cout << endl;
-		cout << "Cup" << '\t';
-		for(int i = 0; i< cup; i++) {
-			cout << "*";
-		}
-		cout << endl;
-	}
-	void setSize() { size--; cup--; water--;}
-	void fillAll() { size = 3; cup = 3; water = 3; }
-	int getWater() { return water;}
-	int getSize() { return size; }
-	int getCup() { return cup; }
-	string getName() { return CoffeeName; }
-};
-
-class Nomal : public Coffee {
-	int cream;
-public:
-	Nomal(string name = "Coffee", int size = 3) : Coffee(name,size) {
-		cream = 3;
-	}
-	
-	void eat() {
-		setSize();
-		cream--;
-	}
-	void fill() {
-		fillAll();
-		cream = 3;
-	}
-	void print() {
-		cout << getName() << '\t';
-		for(int i = 0; i < getSize(); i++) {
-			cout << "*";
-		}
-		cout << endl;
-		cout << "Cream" << '\t';
-		for(int i = 0; i < cream; i++) {
-			cout << "*";
-		}
-		cout << endl;
-		printNo();
+	void setCount(int count) { this->count = count; }
+	int getCount() { return count; }
+	string getName() { return name; }
+	bool eatCount(int count) {  
+			if(this->count <= 0)
+				return false;
+			else 
+				this->count -= count;
+		return true;
 	}
 };
 
-class Black : public Coffee {
-	
+class Coffee : public Product {
 public:
-	Black(string name = "Balck", int size = 3) : Coffee(name, size) {}
-	
-	void eat() {
-		setSize();
-	}
-	
-	void fill() {
-		fillAll();
-	}
-	
-	void print() {
-		cout << getName() << '\t';
-		for(int i = 0; i < getSize(); i++) {
-			cout << "*";
-		}
-		cout << endl;
-		printNo();
+	Coffee() {
+		name = "Coffee";
+		count = 3;
 	}
 };
 
-class Sugar : public Coffee {
-	int sugar;
+class Sugar : public Product {
 public:
-	Sugar(string name = "Sugar", int size = 3) : Coffee(name, size) {
-		sugar = 3;
-	}
-	
-	void fill() {
-		fillAll();
-		sugar = 3;
-	}
-	
-	void eat() {
-		setSize();
-		sugar--;
-	}
-	void print() {
-		cout << getName() << '\t';
-		for(int i = 0; i < getSize(); i++) {
-			cout << "*";
-		}
-		cout << endl;
-		printNo();
+	Sugar() {
+		name = "Sugar";
+		count = 3;
 	}
 };
 
-void start() {
-	cout << "------ 명품 커피 자판기입니다. ------" << endl;
-}
+class CREAM : public Product {
+public:
+	CREAM() {
+		name = "CREAM";
+		count = 3;
+	}
+};
 
-int menu() {
-	int num;
-	cout << "보통 커피:0, 설탕 커피:1, 블랙 커피:2, 채우기:3, 종료:4 >> ";
-	cin >> num;
+class Water : public Product {
+public:
+	Water() {
+		name = "Water";
+		count = 3;
+	}
+};
+
+class Cup : public Product {
+public:
+	Cup() {
+		name = "Cup";
+		count = 3;
+	}
+};
+
+class Machin {
+	Product *p[5];
+public:
+	Machin() {
+		cout << "***** 명품 커피 자판기켭니다 *****" << endl;
+		p[0] = new Coffee();
+		p[1] = new Sugar();
+		p[2] = new CREAM();
+		p[3] = new Water();
+		p[4] = new Cup();
+		printStats();
+	}
 	
-	return num;
-}
+	void printStats() {
+		for(int i = 0; i < 5; i++) {
+			cout << p[i]->getName() << '\t';
+			for(int j = 0; j < p[i]->getCount(); j++)
+				cout << "*";
+			cout << endl;
+		}
+	}
+	
+	void start() {
+		while(true) {
+			int s = menu();
+			if(s == 3) {
+				for(int i = 0; i < 5; i++) {
+					p[i]->setCount(3);
+				}
+				cout << "모든 통을 채웁니다." << endl;
+				printStats();
+				continue;
+			} else if (s == 4) {
+				cout << "프로그램을 종료합니다." << endl;
+				exit(0);
+			}
+			
+			if(p[0]->eatCount(1) == false) {
+				cout << "재료가 부족합니다." << endl;
+				printStats();
+				continue;
+			}
+			if(p[3]->eatCount(1) == false) {
+				cout << "재료가 부족합니다." << endl;
+				printStats();
+				continue;
+			}
+			
+			if(p[4]->eatCount(1) == false) {
+				cout << "재료가 부족합니다." << endl;
+				printStats();
+				continue;
+			}
+			
+			switch(s) {
+				case 0 : 
+					if(p[2]->eatCount(1) == false) {
+						cout << "재료가 부족합니다." << endl;
+						printStats();
+						continue;
+					}
+					
+					cout << "맛있는 보통 커피 나왔습니다." << endl;
+					printStats();
+					break;
+				case 1 :
+					if(p[1]->eatCount(1) == false) {
+						cout << "재료가 부족합니다." << endl;
+						printStats();
+						continue;
+					}
+					
+					cout << "맛있는 설탕 커피 나왔습니다." << endl;
+					printStats();
+					break;
+				case 2 :
+					cout << "맛있는 블랙 커피 나왔습니다." << endl;
+					printStats();
+					break;
+				default :
+					cout << "잘못 입력하셨습니다." << endl;
+					break;
+				
+			}
+		}
+	}
+	
+	int menu() {
+		int num;
+		cout << "보통 커피: 0, 설탕 커피: 1, 블랙 커피: 2, 채우기: 3, 종료 : 4 >> ";
+		cin >> num;
+		return num;
+	}
+};
+
 
 
 int main() {
-	string name;
-	int size;
-	Nomal n;
-	Sugar s;
-	Black b;
-	bool t = true;
-	start();
-	while(t) {
-		
-		n.print();
-		s.print();
-		b.print();
-		int num = menu();
-		if(num == 0) {
-			n.eat();
-		} else if(num == 1) {
-			s.eat();
-		} else if (num == 2) {
-			b.eat();
-		} else if (num == 3) {
-			n.fill();
-			s.fill();
-			b.fill();
-		} else if (num == 4) {
-			break;
-		}
-	}
-	
-
-	
-	
+	cout.setf(ios::left);
+	Machin m;
+	m.start();
 }
